e4cb9ce09a810ded7b78cffae4d63d3d4b373ccd
Cesar Cisneros <cecisner@ucsd.edu>
2014-02-27 13:33:55 -0800

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+    $('.likeBtn').click(analytics);
 }
+
+ function analytics(e) {
+   ga("send","event","like","click");
+ }
